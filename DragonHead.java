bec2c18a7a4eb68119b5436f9ca4ccb88719d70a
DiegoDiazDeLeon <diegodiazdeleoncai@gmail.com>
2020-05-11 00:59:56 -0500

    Add Cabeza de Dragon en Movimiento para Nivel 1

---
+++ DragonHead.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DragonHead here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DragonHead extends Actor
+{
+    private int xVelocity=1;
+    private int xDirection=1;
+    /**
+     * Act - do whatever the DragonHead wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here
+        int x= getX();
+        int y= getY();
+        setLocation(x + xVelocity * xDirection,y);
+        if(isAtEdge()) {
+        xDirection = xDirection *-1;
+        
+        
+    }  
+}
+}
